c3335c8bf1f2d9b98574317809b5f02f88b33a63
Max-Cussans1 <44192221+Max-Cussans1@users.noreply.github.com>
2020-07-11 00:57:57 +0100

    Game Controller
    Basic game controller script - nothing in this yet will just get created and never deleted if it's called from another script if it doesn't already exist.

---
+++ Test/Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [HideInInspector] public int level = 1;
+
+    private static GameController privateInstance;
+
+    public static GameController instance
+    {
+        get
+        {
+            if (privateInstance == null)
+            {
+                //if there is no instance of GameController, create one if/when it's called in script
+                privateInstance = FindObjectOfType<GameController>();
+                if (privateInstance == null)
+                {
+                    privateInstance = new GameObject("Created Game Controller", typeof(GameController)).GetComponent<GameController>();
+                }
+            }
+
+            return privateInstance;
+        }
+        private set
+        {
+            privateInstance = value;
+        }
+    }
+
+    void Awake()
+    {
+        //make sure we don't destroy this instance
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
