55c3ea603d75736258452827d536a1286f44e2ea
Max-Cussans1 <44192221+Max-Cussans1@users.noreply.github.com>
2020-07-11 20:16:01 +0100

    Started enemy aim script
    This is not working as is but started moving variables and functionality over to extend the aimscript class.

--- Test/Assets/Scripts/Enemy.cs
+++ Test/Assets/Scripts/Enemy.cs
@@ -5,68 +5,41 @@ using UnityEngine.Assertions;
 
 public class Enemy : MonoBehaviour
 {
-    public float fireRate = 1.0f;
     public int health = 10;
     public float moveSpeed = 3.0f;
     public float aggroRange = 5.0f;
     public float range = 5.0f;
     public float projectileSpeed = 100.0f;
 
+    public bool lineOfSight = false;
 
     public Rigidbody2D projectile;
 
-    private GameObject player;
-    private BoxCollider2D playerCollider;
-    private BoxCollider2D shieldCollider;
+    public GameObject playerTarget;
+    public BoxCollider2D targetCollider;
+    public BoxCollider2D targetShieldCollider;
     private Vector3 playerPos;
     private float tempFireRate;
     private bool hasFired = false;
-    private bool lineOfSight = false;
+    private Vector3 dir;
 
     void Start()
     {
-        player = GameObject.Find("Player");
-        Assert.IsNotNull(player, "Player was null");
-        playerCollider = player.GetComponent<BoxCollider2D>();
-        shieldCollider = player.GetComponentInChildren<BoxCollider2D>();
-        tempFireRate = fireRate;
+        playerTarget = GameObject.Find("Player");
+        Assert.IsNotNull(playerTarget, "Player was null");
+        targetCollider = playerTarget.GetComponent<BoxCollider2D>();
+        targetShieldCollider = GameObject.Find("Shield").GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        playerPos = player.transform.position;
+    {       
 
-        if(Vector2.Distance(playerPos, transform.position) < aggroRange && lineOfSight)
-        {
-            LookAt(playerPos);
-
-            if (!hasFired)
-            {
-                if(Vector2.Distance(playerPos, transform.position) < range)
-                {
-                    FireForwards();
-                    hasFired = true;
-                }
-            }
-        }
-
-        //check if we've fired
-        if(hasFired)
-        {
-            tempFireRate -= Time.deltaTime;
-            if(tempFireRate < 0)
-            {
-                hasFired = false;
-                tempFireRate = fireRate;
-            }
-        }
-
-        Vector3 dir = playerPos - transform.position;
+        dir = playerPos - transform.position;
         RaycastHit2D hit = Physics2D.Raycast(transform.position, dir);
         Debug.DrawRay(transform.position, dir, Color.green, 0.0f);
 
-        if (hit.collider != playerCollider)
+        if (hit.collider != targetCollider || hit.collider != targetShieldCollider)
         {
             //move around
             lineOfSight = false;
@@ -77,19 +50,10 @@ public class Enemy : MonoBehaviour
         }                       
     }
 
-    private void LookAt(Vector2 targetPos)
-    {    
-        targetPos.x = targetPos.x - transform.position.x;
-        targetPos.y = targetPos.y - transform.position.y;
-
-        float angleToRotate = Mathf.Atan2(targetPos.y, targetPos.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angleToRotate));
-    }
-
-    private void FireForwards()
-    {
-        Rigidbody2D proj;
-        proj = Instantiate(projectile, transform.position, transform.rotation);
-        proj.AddForce(transform.right * projectileSpeed);
-    }
+    //private void FireForwards()
+    //{
+    //    Rigidbody2D proj;
+    //    proj = Instantiate(projectile, transform.position, transform.rotation);
+    //    proj.AddForce(transform.right * projectileSpeed);
+    //}
 }
---
+++ Test/Assets/Scripts/EnemyAimScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+[RequireComponent(typeof(Enemy))]
+public class EnemyAimScript : AimScript
+{
+    public float fireRate = 1.0f;
+    [HideInInspector] public bool canFire = false;
+    Enemy enemy;
+    
+    private GameObject player;
+    private Vector2 playerPos;
+    private Vector2 targetPos;
+    private float tempFireRate = 1.0f;
+
+
+
+    void Start()
+    {
+        enemy = this.GetComponent<Enemy>();
+        tempFireRate = fireRate;
+    }
+
+
+    public override bool ShouldShoot()
+    {
+        return canFire && enemy.lineOfSight;
+    }
+
+    void Update()
+    {
+        playerPos = enemy.playerTarget.transform.position;
+        if (!canFire)
+        {
+            if (Vector2.Distance(playerPos, transform.position) < enemy.range)
+            {
+                canFire = true;
+            }
+        }
+
+        //check if we've fired
+        if (canFire)
+        {
+            tempFireRate -= Time.deltaTime;
+            if (tempFireRate < 0)
+            {
+                canFire = false;
+                tempFireRate = fireRate;
+            }
+        }
+    }
+
+    public override void Aim()
+    {
+        targetPos.x = playerPos.x - transform.position.x;
+        targetPos.y = playerPos.y - transform.position.y;
+
+        float angleToRotate = Mathf.Atan2(targetPos.y, targetPos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angleToRotate));
+    }
+
+}
