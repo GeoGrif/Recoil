37778d3ad8025c992841adbc3e1fb00061e5d45b
Max-Cussans1 <44192221+Max-Cussans1@users.noreply.github.com>
2020-07-11 10:57:54 +0100

    Shield Functionality
    Added functionality in a new script to enabled/disable the shield when holding right mouse button. Added a variable for the shield length but not set up how to change this yet.

---
+++ Test/Assets/Scripts/ShieldScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldScript : MonoBehaviour
+{
+    [SerializeField] private float shieldLength = 1.0f;
+
+    SpriteRenderer spriteRenderer;
+    BoxCollider2D collider;
+    bool enableShield = false;
+    bool shieldIsActive = false;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+        collider = this.GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            enableShield = true;
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            enableShield = false;
+        }
+
+        if (enableShield && !shieldIsActive)
+        {
+            ActivateShield();
+            shieldIsActive = true;
+        }
+
+        if (!enableShield && shieldIsActive)
+        {
+            DeactivateShield();
+            shieldIsActive = false;
+        }
+    }
+
+    void ActivateShield()
+    {
+        spriteRenderer.enabled = true;
+        collider.enabled = true;
+    }
+
+    void DeactivateShield()
+    {
+        spriteRenderer.enabled = false;
+        collider.enabled = false;
+    }
+}
