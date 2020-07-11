1f4a05f66775a5110e03ce2ef9207d2676654b89
Max-Cussans1 <44192221+Max-Cussans1@users.noreply.github.com>
2020-07-11 02:41:57 +0100

    Camera Script
    Simple camera script to follow the player and saved the main camera as a prefab.

---
+++ Test/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class CameraScript : MonoBehaviour
+{
+    GameObject player;
+    public float height = -10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        Assert.IsNotNull(player, "Player was null");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, height);
+    }
+}
