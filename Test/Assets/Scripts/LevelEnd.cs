3b403334a842129ec62c848bfdb43f4d243ec99c
Max-Cussans1 <44192221+Max-Cussans1@users.noreply.github.com>
2020-07-12 15:02:58 +0100

    LevelEnd script
    Added a levelend prefab to transition to a scene. Level index variable is exposed in editor but if the index entered is positive and doesn't exist it will default to the main menu. If it stays at its default value it will just load the next scene.

---
+++ Test/Assets/Scripts/LevelEnd.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelEnd : MonoBehaviour
+{
+    [Tooltip("Use File>build settings for each scene's index")]
+    public int levelIndex = 0;
+
+    private BoxCollider2D player;
+    private LevelLoader levelLoader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<BoxCollider2D>();
+
+        levelLoader = GameObject.Find("LevelLoader").GetComponent<LevelLoader>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider == player)
+        {
+            if (levelIndex > 0)
+            {
+                levelLoader.FadeToLevel(levelIndex);
+            }
+            else if(SceneManager.GetActiveScene().buildIndex + 1 != null)
+            {
+                levelLoader.FadeToNextLevel();
+            }
+            else
+            {
+                //default to main menu if the levelindex doesn't exist
+                Debug.Log("Ensure that the level index exists in file>build settings");
+                levelLoader.FadeToLevel(1);
+            }
+        }
+    }
+}
