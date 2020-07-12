61d3b8d947214eedd58ba433d2e7dc7b109bc3df
Max-Cussans1 <44192221+Max-Cussans1@users.noreply.github.com>
2020-07-12 18:25:25 +0100

    Cant die Loading Next Level
    Added a static bool to loadlevel to check if we're loading the next scene to make sure we can't die.

--- Test/Assets/Scripts/PlayerScript.cs
+++ Test/Assets/Scripts/PlayerScript.cs
@@ -36,7 +36,7 @@ public class PlayerScript : MonoBehaviour
 
     private void Update()
     {
-        if (health <= 0)
+        if (health <= 0 && !LevelLoader.loadingNextLevel)
         {
             CameraScript.shakeDuration = 0f;
 
