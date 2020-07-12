1318136860ab1f20e2cc9c91ce3bc642274b96aa
Max-Cussans1 <44192221+Max-Cussans1@users.noreply.github.com>
2020-07-12 15:45:21 +0100

    Projectile tweaks and taking damage audio
    Added bounce count to projectiles and now destroying them after max bounces value. Also exposed an audioclip variable for the player taking damage in playerscript.

--- Test/Assets/Scripts/Projectile.cs
+++ Test/Assets/Scripts/Projectile.cs
@@ -12,6 +12,10 @@ public class Projectile : MonoBehaviour
     [SerializeField] private float ExplosiveRange = 0.1f;
     [SerializeField] private float ExplosivePower = 10f;
     [SerializeField] public float Damage = 10.0f;
+    [SerializeField] private float force = 100.0f;
+
+    private int numberOfBounces = 0;
+    [SerializeField] private int maxNumberOfBounces = 10;
 
     [SerializeField] private AudioClip explosionSound;
     [SerializeField] private AudioClip ricochetSound;
@@ -69,9 +73,23 @@ public class Projectile : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        numberOfBounces++;
+        if(numberOfBounces >= maxNumberOfBounces)
+        {
+            Destroy(this.gameObject);
+        }
         //if there is a collision and it's with this object. 
         if (collision.collider.gameObject.tag == "Projectile" && collision.otherCollider.gameObject.tag == "Projectile")
         {
+
+            Rigidbody2D rb;
+
+            Vector2 dir = new Vector2(collision.contacts[0].point.x - transform.position.x, collision.contacts[0].point.y - transform.position.y);
+            dir = dir.normalized;
+
+            rb = collision.gameObject.GetComponent<Rigidbody2D>();
+            rb.AddForce(dir * force);
+            AudioManager.instance.PlaySFX(ricochetSound);
 
             //Vector3 explosionPos = transform.position;
 
@@ -106,24 +124,9 @@ public class Projectile : MonoBehaviour
 
             //        rb.AddForce(forceDirection, ForceMode2D.Impulse);
             //    }
-                  
+
             //}
 
-            if (checkForMinVelocity && (rb.velocity.x < minVelocity && rb.velocity.x > -minVelocity) && (rb.velocity.y < minVelocity && rb.velocity.y > -minVelocity))
-            {
-                tempTimeToDestroy -= Time.deltaTime;
-                if (tempTimeToDestroy < 0)
-                {
-                    Destroy(this.gameObject);
-                }
-            }
-            else
-            {
-                tempTimeToDestroy = timeToDestroy;
-            }
-
-            Destroy(collision.otherCollider.gameObject);
-            Destroy(collision.collider.gameObject);
         }
         else if(collision.collider.gameObject.name != "Shield" || collision.collider.gameObject.tag != "Player" || collision.collider.gameObject.tag != "Enemy")
         {
