b0f8f0cf289890375bb2b511a17cbdab2e599e39
GrubboGremlin <daleyaylor537@gmail.com>
2020-10-04 20:55:48 +0100

    added some commenting
    added some commenting to the base AimScript and PlayerAimScript.

--- Test/Assets/Scripts/AimScript.cs
+++ Test/Assets/Scripts/AimScript.cs
@@ -5,28 +5,40 @@ using UnityEngine;
 
 public abstract class AimScript : MonoBehaviour
 {
-
-    /**
-    * the position the object this script is aattached to is currently at.  
-    */
+    /// <summary>
+    ///the position the object this script is attached to is currently at.  
+    /// </summary>
     [SerializeField]  private Vector3 ObjectPosition;
 
-    //add our projectile prefab in editor
+    /// <summary>
+    ///add our projectile prefab in editor
+    /// </summary>
     [SerializeField] public Rigidbody2D ProjectileObject;
 
+    /// <summary>
+    /// the initial speed of the projectile when fired. 
+    /// </summary>
     [SerializeField]  public float projectileSpeed = 100.0f;
 
+
+    /// <summary>
+    /// the sound made when a projectile is fired.
+    /// </summary>
     [SerializeField] private AudioClip shootSound;
 
-
+    /// <summary>
+    ///  the force applied to the object that is firing the projectile. 
+    /// </summary>
     public float playerPushSpeed = 300.0f;
 
-
+    /// <summary>
+    /// A time span to hold the current amount of charge.  
+    /// </summary>
     public float _TimeSpan;
 
-    /**
-    * the time in seconds to full charge 
-    */
+    /// <summary>
+    /// the time in seconds to full charge 
+    /// </summary>
     [SerializeField] public float TotalChargeTime = 0.5f;
 
 
@@ -46,20 +58,21 @@ public abstract class AimScript : MonoBehaviour
         }
     }
 
-    /**
-    * weather the object this script is attached to should shoot or not.  
-    */
+    /// <summary>
+    /// weather the object this script is attached to should shoot or not.  <code>true</code> if the object should shoot.
+    /// </summary>
+    /// <returns> <code>true</code> if the object should shoot. else <code>false</code>.</returns>
     public abstract bool ShouldShoot();
 
-    /**
-    * Methed used to set current aim position of this object 
-    */
+    /// <summary>
+    /// Methed used to set the current aim position of this object 
+    /// </summary>
     public abstract void Aim();
- 
 
-    /**
-    * method called when a projectile is fired.  
-    */
+
+    /// <summary>
+    /// method called when a projectile is fired.  
+    /// </summary>
     private void Shoot()
     {
         Rigidbody2D projectile;
